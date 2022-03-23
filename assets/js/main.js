e6fa147115fdc66335bc4a5c3cf88009b1b530ea
MokkaCobain <91149724+MokkaCobain@users.noreply.github.com>
2022-03-23 13:27:59 +0100

    Ajout du style au converter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,15 @@
 //Les variables utiles
 let temperature = document.getElementById(`temperature`); // Saisie user
-
 let converter = document.converter; // Name du formulaire
 
-let formule = (temperature.value * 9/5) + 32; // Le calcul de la conversion
-
-let fahrenheit = `${formule}°F`; // Le résultat en fahrenheit
-
-let resultat = `${temperature.value}°C = ${fahrenheit}`; // La phrase du résultat
 
 // La div du résultat
 let divResultat = document.createElement(`div`);
 divResultat.setAttribute(`id`, `reponse`);
 
 // Placement de la div du résultat
-let divContainer = document.getElementById(`container`);
-divContainer.appendChild(divResultat);
+let divContent = document.getElementById(`content`);
+divContent.appendChild(divResultat);
 
 // EVENT = vérifier la saisie utilisateur
 temperature.addEventListener(`keyup`, function (e){
@@ -33,9 +27,19 @@ temperature.addEventListener(`keyup`, function (e){
 // EVENT = convertir la saisie utilisateur 
 
 converter.addEventListener(`submit`, function(e){
-    
+
+    // Le calcul de la conversion
+    let formule = (temperature.value * 9/5) + 32; 
+    // Le résultat
+    let fahrenheit = `${formule}°F`;
+    // La phrase du résultat
+    let resultat = `${temperature.value}°C = ${fahrenheit}`; 
+
+    //Affichage du résultat
     divResultat.className = `bonne-saisie`;
     divResultat.innerText = `${resultat}`;
 
     e.preventDefault();
+
+   
 });
